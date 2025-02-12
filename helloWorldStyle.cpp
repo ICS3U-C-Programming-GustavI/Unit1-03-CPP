c8d60594a9f1bbe2f72a915d0cf5385fc9c19ca0
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-02-12 23:04:12 +0000

    Commit message

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Gustav I All rights reserved.
+// Created by Gustav I.
+// Date : Feb 11, 2025
+// This program displays "Hello,World with style."
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std :: endl;
+    std::cout << "C++ is a great coding language!" << std ::endl;
+}
+
